52499fbd7287f1e43c4b409de65753de728e0e49
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-04-25 01:44:54 +0700

    add: question 122)

---
+++ LeetCode/c++/122_bestTimeToBuyStock_2.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        std::ios::sync_with_stdio(false);
+        cin.tie(0);
+        cout.tie(0);
+
+        int total = 0;
+        int maxProfit = 0;
+        int minPrice = INT_MAX;
+
+        for (size_t i = 0; i < prices.size(); ++i)
+        {
+            minPrice = min(minPrice, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - minPrice);
+
+            if (i+1 < prices.size() && prices[i+1] < prices[i])
+            {
+                minPrice = prices[i + 1];
+                total += maxProfit;
+                maxProfit = 0;
+
+                continue;
+            } else if (i+1 == prices.size() && prices.size() > 1 && prices[i] >= prices[i-1]) {
+                total += maxProfit;
+            }
+        }
+
+        return total;
+    }
+};
+
+int main()
+{
+    vector<int> prices;
+    prices.push_back(1);
+    prices.push_back(9);
+    prices.push_back(6);
+    prices.push_back(9);
+    prices.push_back(1);
+    prices.push_back(7);
+    prices.push_back(1);
+    prices.push_back(1);
+    prices.push_back(5);
+    prices.push_back(9);
+    prices.push_back(9);
+    prices.push_back(9);
+
+    Solution sol;
+
+    cout << sol.maxProfit(prices) << endl;
+    return 0;
+}
