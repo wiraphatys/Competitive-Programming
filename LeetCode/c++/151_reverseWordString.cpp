3b17fd9818ff85803a3370eeebcdc7ba3df9faac
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-05-03 21:28:40 +0700

    add: 151)

--- LeetCode/c++/151_reverseWordString.cpp
+++ LeetCode/c++/151_reverseWordString.cpp
@@ -1,7 +1,32 @@
-class Solution
-{
-public
-    String reverseWords(String s)
-    {
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans = "";
+
+        if (s.length() == 1) return s;
+        
+        int idx = 0;
+        string temp = "";
+        while (idx <= (int)s.length()) {
+            if (temp.length() != 0 && (idx == (int)s.length() || s[idx] == ' ')) {
+                ans = temp + ' ' + ans;
+                temp = "";
+            } else if (s[idx] != ' ') temp += s[idx];
+            idx++;
+        }
+        ans.pop_back();
+
+        return ans;
+
     }
+};
+
+int main() {
+    Solution sol;
+
+    cout << sol.reverseWords("hello world");
+    return 0;
 }
