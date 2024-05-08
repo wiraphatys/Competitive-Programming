b4c2a37d92c37ec47dff82e39c0adf08add3fcb9
wiraphat <132466647+wiraphatys@users.noreply.github.com>
2024-05-08 09:58:49 +0700

    update: 238)

--- LeetCode/c++/238_productArrayEcceptSelf.cpp
+++ LeetCode/c++/238_productArrayEcceptSelf.cpp
@@ -4,36 +4,30 @@ using namespace std;
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int> &nums) {
-        int countZero = 0;
-        int n = nums.size();
-        int total = 1;
-        vector<bool> isZero(n, false);
-        vector<int> ans(n, 1);
-
-        for (int i = 0; i < n; ++i) {
-            if (nums[i] == 0) {
-                countZero++;
-                isZero[i] = true;
-                continue;
-            }
-            total *= nums[i];
+        size_t n = nums.size();
+        int sum = 1;
+        vector<int> prefixProd(n, 0);
+        for (size_t i = 0; i < n; ++i) {
+            sum *= nums[i];
+            prefixProd[i] = sum;
         }
 
-        for (int i = 0; i < n; ++i) {
-            if (countZero > 1) {
-                ans[i] = 0;
-            } else if (countZero == 1) {
-                if (isZero[i]) {
-                    ans[i] = total;
-                } else {
-                    ans[i] = 0;
-                }
-            } else {
-                ans[i] = total / nums[i];
-            }
+        sum = 1;
+        vector<int> suffixProd(n, 0);
+        for (int i = n-1; i >= 0; --i) {
+            sum *= nums[i];
+            suffixProd[i] = sum;
+        }
+
+        vector<int> ans(n, 0);
+        for (size_t i = 0; i < n; ++i) {
+            ans[i] = (i > 0 ? prefixProd[i-1] : 1) * (i < n-1 ? suffixProd[i+1] : 1);
         }
 
         return ans;
     }
 };
 
+//  1  2  3  4
+//  1  2  6 24
+// 24 24 12  4
